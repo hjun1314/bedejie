7cfbcae87a18df467087befff1f93d4a9036ab37
Jerry <huangjun0529@126.com>
2017-10-30 10:52:11 +0800

    控制广告时间

--- budejie/budejie/Classes/AD(广告)/Controller/HJADVC.m
+++ budejie/budejie/Classes/AD(广告)/Controller/HJADVC.m
@@ -8,7 +8,7 @@
 
 #import "HJADVC.h"
 #import "HJADItemModel.h"
-
+#import "HJTabVC.h"
 #define code2 @"phcqnauGuHYkFMRquANhmgN_IauBThfqmgKsUARhIWdGULPxnz3vndtkQW08nau_I1Y1P1Rhmhwz5Hb8nBuL5HDknWRhTA_qmvqVQhGGUhI_py4MQhF1TvChmgKY5H6hmyPW5RFRHzuET1dGULnhuAN85HchUy7s5HDhIywGujY3P1n3mWb1PvDLnvF-Pyf4mHR4nyRvmWPBmhwBPjcLPyfsPHT3uWm4FMPLpHYkFh7sTA-b5yRzPj6sPvRdFhPdTWYsFMKzuykEmyfqnauGuAu95Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiu9mLfqHbD_H70hTv6qnHn1PauVmynqnjclnj0lnj0lnj0lnj0lnj0hThYqniuVujYkFhkC5HRvnB3dFh7spyfqnW0srj64nBu9TjYsFMub5HDhTZFEujdzTLK_mgPCFMP85Rnsnbfknbm1QHnkwW6VPjujnBdKfWD1QHnsnbRsnHwKfYwAwiuBnHfdnjD4rjnvPWYkFh7sTZu-TWY1QW68nBuWUHYdnHchIAYqPHDzFhqsmyPGIZbqniuYThuYTjd1uAVxnz3vnzu9IjYzFh6qP1RsFMws5y-fpAq8uHT_nBuYmycqnau1IjYkPjRsnHb3n1mvnHDkQWD4niuVmybqniu1uy3qwD-HQDFKHakHHNn_HR7fQ7uDQ7PcHzkHiR3_RYqNQD7jfzkPiRn_wdKHQDP5HikPfRb_fNc_NbwPQDdRHzkDiNchTvwW5HnvPj0zQWndnHRvnBsdPWb4ri3kPW0kPHmhmLnqPH6LP1ndm1-WPyDvnHKBrAw9nju9PHIhmH9WmH6zrjRhTv7_5iu85HDhTvd15HDhTLTqP1RsFh4ETjYYPW0sPzuVuyYqn1mYnjc8nWbvrjTdQjRvrHb4QWDvnjDdPBuk5yRzPj6sPvRdgvPsTBu_my4bTvP9TARqnam"
 @interface HJADVC ()
 @property (weak, nonatomic) IBOutlet UIImageView *launchImageView;
@@ -17,6 +17,9 @@
 @property (nonatomic,strong)HJADItemModel *item;
 
 @property (nonatomic,strong)UIImageView *adImageView;
+
+@property (nonatomic,strong)NSTimer *timer;
+@property (weak, nonatomic) IBOutlet UIButton *jumpBtn;
 
 
 @end
@@ -55,6 +58,11 @@
     }
     
 }
+- (IBAction)clickJumpBtn:(UIButton *)sender {
+    HJTabVC *tab = [[HJTabVC alloc]init];
+    UIApplication.sharedApplication.keyWindow.rootViewController = tab;
+    [_timer invalidate];
+}
 
 - (void)loadADdata{
     
@@ -84,8 +92,19 @@
 
     }];
     
-}
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timeChange) userInfo:nil repeats:YES];
     
+    
+}
+- (void)timeChange{
+    static int i = 3;
+    
+    if (i == 0) {
+        [self clickJumpBtn:nil];
+    }
+    i--;
+    [_jumpBtn setTitle:[NSString stringWithFormat:@"跳转(%d)",i] forState:UIControlStateNormal];
+}
 
 - (void)setupLaunchImage{
     
